47acfec5bb1c5f4a912bebf255d8bf9d3bd23cf7
Mohd Haider <120472415+mahider78672@users.noreply.github.com>
2025-09-26 23:42:16 +0530

    Create NaiveSolution.java

---
+++ Day5|100: Plus One/NaiveSolution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int index =  digits.length-1;
+        if(digits[index] != 9){
+            digits[index]++;
+            return digits;
+        }
+        int carry = 0;
+        while(index >= 0 && digits[index] ==9){
+            digits[index] =0;
+            carry = 1;    
+            index--;
+        }
+        if(index == -1){
+            int[] temp = new int[digits.length+1];
+            temp[0] = 1;
+            return temp;
+        }
+        digits[index]++;
+        return digits;
+    }
+}
