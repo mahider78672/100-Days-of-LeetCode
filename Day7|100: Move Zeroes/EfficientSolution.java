02460388f3d22d90b3d59af7b806a86c81367cb3
Mohd Haider <120472415+mahider78672@users.noreply.github.com>
2025-09-28 20:38:25 +0530

    Create EfficientSolution.java

---
+++ Day7|100: Move Zeroes/EfficientSolution.java
@@ -0,0 +1,15 @@
+class EfficientSolution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                if(i != index){
+                    int temp = nums[i];
+                    nums[i] = nums[index];
+                    nums[index] = temp;
+                }
+                index++;
+            }
+        }
+    }
+}
