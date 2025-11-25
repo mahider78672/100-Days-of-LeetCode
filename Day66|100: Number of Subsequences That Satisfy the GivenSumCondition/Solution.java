8150d36f6f2d0784199fb98c28535896b9183304
Mohd Haider <120472415+mahider78672@users.noreply.github.com>
2025-11-25 23:43:29 +0530

    Create Solution.java

---
+++ Day66|100: Number of Subsequences That Satisfy the GivenSumCondition/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int MOD = 1_000_000_007;
+        int n = nums.length;
+        
+        // Sort the array
+        Arrays.sort(nums);
+        
+        // Precompute powers of 2
+        int[] pow = new int[n];
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = (pow[i - 1] * 2) % MOD;
+        }
+        
+        int left = 0, right = n - 1;
+        int result = 0;
+        
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                // All subsequences with nums[left] as min and any element
+                // from left to right as max
+                result = (result + pow[right - left]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return result;
+    }
+}
